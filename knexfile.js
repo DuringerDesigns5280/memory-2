458007f50e77f4fd554b209f27c657eb24792f10
Taylor <tayjed2010@gmail.com>
2018-07-10 14:25:49 -0600

    fixed production

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,3 @@
-// Update with your config settings.
 
 module.exports = {
 
@@ -8,36 +7,9 @@ module.exports = {
     
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
 
+  }
 };
